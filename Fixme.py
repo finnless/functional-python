7a867184d12da16e1f3d02c32a42a1f0ec61a56a
finnless <nolan@nolanwindham.com>
2023-01-30 10:10:10 -0800

    HW2 functional programing problems wrote Fixme.py to pass unittests

--- Fixme.py
+++ Fixme.py
@@ -20,6 +20,7 @@ def evens(n):
     >>> evens(-1)
     []
     '''
+    return list(filter(lambda x: x%2==0, range(0,n+1)))
 
 
 def threes(n):
@@ -37,6 +38,7 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
+    return list(filter(lambda x: '3' in str(x), range(0,n+1)))
 
 
 def small_words(text):
@@ -55,6 +57,7 @@ def small_words(text):
     >>> small_words('a big word is bad')
     ['a', 'big', 'word', 'is', 'bad']
     '''
+    return list(filter(lambda x: len(x) < 5, text.split()))
 
 
 def squares(n):
@@ -71,6 +74,7 @@ def squares(n):
     >>> squares(10)
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
+    return list(map(lambda x: x**2, range(1, n+1)))
 
 
 def lengths(strings):
@@ -84,3 +88,4 @@ def lengths(strings):
     >>> lengths(['this','is','a','test'])
     [4, 2, 1, 4]
     '''
+    return list(map(lambda string: len(string), strings))
